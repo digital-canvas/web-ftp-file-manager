d261caefc945abb5f8667ce79f8f015ee061dc10
Jonathan Bernardi <jonathan@digitalcanvas.com>
2019-12-10 11:55:43 -0800

    add comments to sample config fi;e

--- system/configs/config.sample.php
+++ system/configs/config.sample.php
@@ -1,4 +1,12 @@
 <?php
+// Displayed in the header for all pages
 $config['app']['name'] = 'Web FTP Manager';
-$config['ftp']['name'] = 'localhost';
+
+// The ftp server hostname that the application connects to
 $config['ftp']['server'] = 'localhost';
+
+// This is the displayed value for the server hostname.
+// This might be different that the actual hostname that is connected to
+// For example the connection might need to be made with an ip address but you want to display a domain to the user.
+$config['ftp']['name'] = 'localhost';
+
